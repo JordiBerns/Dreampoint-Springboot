87a16b42e9a53fb75913eb4891413be99b158c65
ContraMund1 <mr_momo2003@hotmail.com>
2019-05-06 10:32:27 +0200

    Removed try catch statement and replaced with if check

--- src/main/java/nl/yer/middlemen/dreampoint/api/PlayerController.java
+++ src/main/java/nl/yer/middlemen/dreampoint/api/PlayerController.java
@@ -2,7 +2,10 @@ package nl.yer.middlemen.dreampoint.api;
 
 import nl.yer.middlemen.dreampoint.character.Player;
 import nl.yer.middlemen.dreampoint.service.PlayerService;
+import org.aspectj.apache.bcel.util.Play;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/players")
@@ -22,6 +25,47 @@ public class PlayerController {
     @PostMapping
     public Player create(@RequestBody Player player) {
         return this.playerService.save(player);
+    }
+
+    @GetMapping("{id}")
+    public  Player get(@PathVariable long id){
+
+        Optional<Player> optionalResult = this.playerService.findById(id);
+        if(optionalResult.isPresent()){
+            Player result = optionalResult.get();
+
+            return result;
+        }
+        else {
+            return null;
+        }
+    }
+
+    @PutMapping("{id}")
+    public Player update(@PathVariable long id, @RequestBody Player input){
+
+        Optional<Player> optionalTarget = this.playerService.findById(id);
+        if(optionalTarget.isPresent()){
+            Player target = optionalTarget.get();
+
+            target.setName(input.getName());
+            target.setAge(input.getAge());
+            target.setHairColour((input.getHairColour()));
+
+            return this.playerService.save(target);
+        }
+
+        else{
+            return null;
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable long id){
+     Optional<Player> optionalResult = this.playerService.findById(id);
+     if(optionalResult.isPresent()){
+         this.playerService.deleteById(id);
+     }
     }
 
 }
