c9a6437b956b05868f1905df47308ceaa5006d7e
Jordi Berns <jordi_berns@live.nl>
2019-05-06 19:59:04 +0200

    change determineIfCanShoot method to be divided under subclasses, not in the parent class.

--- src/main/java/nl/yer/middlemen/dreampoint/character/BigEnemy.java
+++ src/main/java/nl/yer/middlemen/dreampoint/character/BigEnemy.java
@@ -1,5 +1,7 @@
 package nl.yer.middlemen.dreampoint.character;
 
+import static nl.yer.middlemen.dreampoint.game.Game.hiScore;
+
 public class BigEnemy extends Enemy {
 
     private long id;
@@ -8,6 +10,21 @@ public class BigEnemy extends Enemy {
         this.setCanDamage(true);
         this.setCanTakeAmountOfShots(2);
         this.setAddToHiScore(20);
+    }
+
+    @Override
+    public boolean determineIfCanShoot() {
+        if (this.isCanDamage()) {
+            if (this.getCanTakeAmountOfShots() > 1) {
+                this.setCanTakeAmountOfShots(this.getCanTakeAmountOfShots() - 1);
+                return false;
+            } else {
+                hiScore += this.getAddToHiScore();
+                return true;
+            }
+        } else {
+            return false;
+        }
     }
 
     @Override
--- src/main/java/nl/yer/middlemen/dreampoint/item/Shotgun.java
+++ src/main/java/nl/yer/middlemen/dreampoint/item/Shotgun.java
@@ -3,8 +3,17 @@ package nl.yer.middlemen.dreampoint.item;
 public class Shotgun extends Weapon {
     private long id;
 
+    public Shotgun() {
+        this.setCanDamage(false);
+    }
+
     @Override
     public void shoot() { }
+
+    @Override
+    public boolean determineIfCanShoot() {
+        return false;
+    }
 
     @Override
     public long getId() {
